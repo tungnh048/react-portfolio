a28aa88189592b0b2252896b953ac8e590908a3f
tungnh048 <tungnh048@gmail.com>
2022-06-26 22:48:50 -0400

    Added Home Page
    finished homepage's animations

---
+++ src/components/Home/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    const nameArray = ['u', 'n', 'g', ' ', 'n', 'g', 'u', 'y', 'e', 'n']
+    const jobArray = ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'e', 'r',]
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br/>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+                <br/>
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={24}/>
+                </h1>
+                <h2>Backend Developer / Frontend Developer / Database Engineer / Post-grad Student</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div> 
+        </div>
+    )
+}
+
+export default Home
