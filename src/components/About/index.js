90e3cf9c2c38d3ab053e78b658b3beb32bc1d080
tungnh048 <tungnh048@gmail.com>
2022-08-07 16:11:54 -0400

    Added Logos to animation on About Page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,12 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJava, faReact } from '@fortawesome/free-brands-svg-icons'
-import { faC } from '@fortawesome/free-solid-svg-icons'
 import AssemblyLogo from '../../assets/images/assembly-logo.png'
+import JavaLogo from '../../assets/images/java-logo.png'
+import CLogo from '../../assets/images/c-logo.png'
+import CPlusPlusLogo from '../../assets/images/cplusplus-logo.png'
+import CSharpLogo from '../../assets/images/csharp-logo.png'
+import ReactLogo from '../../assets/images/react-logo.png'
 
 const About = () => {
     
@@ -27,38 +29,66 @@ const About = () => {
                     />
                 </h1>
                 <p>
-                    I am a very ambitious recent grad student that is looking for a role in 
+                    I am a very ambitious student and a recent college graduate. Currently, I am looking for a role in 
                     an established IT company for the opportunity to work with the latest technologies
                     on challenging and diverse projects.
                 </p>
                 <p>
-                    I am loyal, dependable, confident, curious and always looking for more opportunity 
-                    to improve myself one step at a time.
+                    I am confident in my ability to adapt to most situations, always curious to learn more, dependable as a worker 
+                    and always try to improve myself one step at a time.
                 </p>
-                <p>
-                    alllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                </p>
+                <p> If I have to describe myself in 3 words, those will be: </p>
+                    
+                    <h3>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['T', 'R', 'U', 'S', 'T', 'W', 'O', 'R', 'T', 'H', 'Y']}
+                            idx={15}
+                        />    
+                    </h3> 
+                        <p>My TRUSTWORTHINESS means that I plan to stay working with you for a lengthy amount of time. I will be a positive role model for the company</p>
+                    <h3>
+                    <h3>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['S', 'E', 'L', 'F', '-', 'M', 'O', 'T', 'I', 'V', 'A', 'T', 'E', 'D']}
+                            idx={15}
+                        />    
+                    </h3> 
+                    </h3>
+                        <p>My levels of SELF-MOTIVATION mean that I will come into the role and I will dedicate the time needed to learn the job quickly and to make sure I do it
+                        to a high standard with little supervision.</p>
+                    <h3>
+                    <h3>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['D', 'E', 'P', 'E', 'N', 'D', 'A', 'B', 'L', 'E']}
+                            idx={15}
+                        />    
+                    </h3> 
+                    </h3>
+                        <p> Finally, my DEPENDABLITY means that I will never let you down and I will be available to work at short notive if needed to help out.</p>
             </div>
 
             <div className="stage-cube-cont">
                 <div className="cubespinner">
                     <div className="face1">
-                        <FontAwesomeIcon icon={faJava} color="#DD0031" />
+                        <img src={JavaLogo} alt="Java" height="190" width="190"/>
                     </div>
                     <div className="face2">
-                        <FontAwesomeIcon icon={faC} color="#F06529" />
+                        <img src={CLogo} alt="C" height="190" width="190"/>
                     </div>
                     <div className="face3">
-                        <FontAwesomeIcon icon={faC} color="#28A4D9" />++
+                        <img src={CPlusPlusLogo} alt="C++" height="190" width="190"/>
                     </div>
                     <div className="face4">
-                        <FontAwesomeIcon icon={faC} color="#5ED4F4" />#
+                        <img src={CSharpLogo} alt="C#" height="190" width="190"/>
                     </div>
                     <div className="face5">
-                        <FontAwesomeIcon icon={faReact} color="#EFD81D" />
+                        <img src={ReactLogo} alt="React" height="190" width="190"/>
                     </div>
                     <div className="face6">
-                        <img src={AssemblyLogo} alt="Assembly" width="200" height="200"/>
+                        <img src={AssemblyLogo} alt="Assembly" height="190" width="190"/>
                     </div>
                 </div>
             </div>
