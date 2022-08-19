9356b7db37e22b30408ddec780e5f5a173563f39
tungnh048 <tungnh048@gmail.com>
2022-08-19 03:32:47 -0400

    update deployment config

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,8 +1,6 @@
 import './index.scss';
 import LogoS from '../../../assets/images/logo-s.png'
 import { useEffect, useRef } from 'react';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import gsap from 'gsap-trial';
 
 const Logo = () => {
     
